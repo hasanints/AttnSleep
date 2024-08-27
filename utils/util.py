4935b8e99deb59f59823e4df6e1ffbf72c2f3a90
hasanints <18320020@std.stei.itb.ac.id>
2024-08-27 10:48:47 +0700

    update train_Kfold_CV.py

--- utils/util.py
+++ utils/util.py
@@ -21,46 +21,28 @@ def load_folds_data_shhs(np_data_path, n_folds):
         folds_data[fold_id] = [training_files, subject_files]
     return folds_data
 
-ddef load_folds_data(np_data_path, n_folds):
+def load_folds_data(np_data_path, n_folds):
     files = sorted(glob(os.path.join(np_data_path, "*.npz")))
-    print("Total files found:", len(files))  # Debug print
-
+    print("Total files found:", len(files))
     if "78" in np_data_path:
         r_p_path = r"utils/r_permute_78.npy"
     else:
         r_p_path = r"utils/r_permute_20.npy"
-
     if not os.path.exists(r_p_path):
         raise FileNotFoundError(f"Permutation file not found: {r_p_path}")
-
     r_permute = np.load(r_p_path)
-    print("Permutation indices:", r_permute)  # Debug print
-
+    print("Permutation indices:", r_permute)
     if len(r_permute) > len(files):
         raise ValueError("Permutation index exceeds the number of available files.")
-
-    files_dict = {}
-    for i in files:
-        file_name = os.path.split(i)[-1]
-        file_num = file_name[3:5]
-        if file_num not in files_dict:
-            files_dict[file_num] = [i]
-        else:
-            files_dict[file_num].append(i)
-
-    files_pairs = [files_dict[key] for key in sorted(files_dict)]
-    files_pairs = np.array(files_pairs, dtype=object)
-
+    files_dict = {os.path.split(i)[-1][3:5]: files_dict.get(os.path.split(i)[-1][3:5], []) + [i] for i in files}
+    files_pairs = np.array([files_dict[key] for key in sorted(files_dict)], dtype=object)
     if len(r_permute) > len(files_pairs):
         raise ValueError("Permutation index exceeds the number of file groups.")
-
     files_pairs = files_pairs[r_permute]
-
     train_files = np.array_split(files_pairs, n_folds)
     folds_data = {}
     for fold_id in range(n_folds):
-        subject_files = train_files[fold_id]
-        subject_files = [item for sublist in subject_files for item in sublist]
+        subject_files = [item for sublist in train_files[fold_id] for item in sublist]
         files_pairs2 = [item for sublist in files_pairs for item in sublist]
         training_files = list(set(files_pairs2) - set(subject_files))
         folds_data[fold_id] = [training_files, subject_files]
@@ -69,45 +51,28 @@ ddef load_folds_data(np_data_path, n_folds):
 
 def calc_class_weight(labels_count):
     total = np.sum(labels_count)
-    class_weight = dict()
-    num_classes = len(labels_count)
-
-    factor = 1 / num_classes
-    mu = [factor * 1.5, factor * 2, factor * 1.5, factor, factor * 1.5] # THESE CONFIGS ARE FOR SLEEP-EDF-20 ONLY
-
-    for key in range(num_classes):
-        score = math.log(mu[key] * total / float(labels_count[key]))
-        class_weight[key] = score if score > 1.0 else 1.0
-        class_weight[key] = round(class_weight[key] * mu[key], 2)
-
-    class_weight = [class_weight[i] for i in range(num_classes)]
-
-    return class_weight
+    class_weight = {key: round(math.log((1 / len(labels_count) * 1.5) * total / float(labels_count[key])) * (1 / len(labels_count) * 1.5), 2) if math.log((1 / len(labels_count) * 1.5) * total / float(labels_count[key])) > 1.0 else 1.0 for key in range(len(labels_count))}
+    return [class_weight[i] for i in range(len(labels_count))]
 
 
 def ensure_dir(dirname):
     dirname = Path(dirname)
     if not dirname.is_dir():
-        dirname.mkdir(parents=True, exist_ok=False)
-
+        dirname.mkdir(parents=True, exist_ok=True)
 
 def read_json(fname):
     fname = Path(fname)
     with fname.open('rt') as handle:
         return json.load(handle, object_hook=OrderedDict)
 
-
 def write_json(content, fname):
     fname = Path(fname)
     with fname.open('wt') as handle:
         json.dump(content, handle, indent=4, sort_keys=False)
 
-
 def inf_loop(data_loader):
-    ''' wrapper function for endless data loader. '''
     for loader in repeat(data_loader):
         yield from loader
-
 
 class MetricTracker:
     def __init__(self, *keys, writer=None):
