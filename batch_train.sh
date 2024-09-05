cac1501d32f0eeae41635886509af18506f97aa4
hasanints <18320020@std.stei.itb.ac.id>
2024-09-05 14:27:59 +0700

    Update batch_train.sh

--- batch_train.sh
+++ batch_train.sh
@@ -1,11 +1,11 @@
 gpu=$1
 data=$2
 
-# Menggunakan Python untuk membaca nilai num_folds
-end=$(python3 -c "import json; print(json.load(open('config.json'))['data_loader']['args']['num_folds'])")
+start=0
+end=`cat config.json | jq '.data_loader.args.num_folds'`
 end=$((end-1))
 
 for i in $(eval echo {$start..$end})
 do
-    python3 train_Kfold_CV.py --fold_id=$i --device $gpu --np_data_dir $data
+   python3 train_Kfold_CV.py --fold_id=$i --device $gpu --np_data_dir $data
 done
