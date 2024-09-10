98dd3e8f75b51ac5d456b52ea066530788074722
hasanints <18320020@std.stei.itb.ac.id>
2024-09-10 08:57:10 +0700

    update

--- base/base_trainer.py
+++ base/base_trainer.py
@@ -198,11 +198,11 @@ class BaseTrainer:
         for root, dirs, files in os.walk(save_dir):
             for file in files:
                 if "outs" in file:
-                     outs_list.append(os.path.join(root, file))
+                    outs_list.append(os.path.join(root, file))
                 if "trgs" in file:
-                     trgs_list.append(os.path.join(root, file))
+                    trgs_list.append(os.path.join(root, file))
 
-        if len(outs_list)==self.config["data_loader"]["args"]["num_folds"]:
+        if len(outs_list) == self.config["data_loader"]["args"]["num_folds"]:
             for i in range(len(outs_list)):
                 outs = np.load(outs_list[i])
                 trgs = np.load(trgs_list[i])
@@ -212,6 +212,19 @@ class BaseTrainer:
         all_trgs = np.array(all_trgs).astype(int)
         all_outs = np.array(all_outs).astype(int)
 
+        # --- Tambahkan Kode di Sini untuk Menghitung dan Menampilkan Metrik ---
+        classification_report_result = classification_report(all_trgs, all_outs)
+        accuracy = accuracy_score(all_trgs, all_outs)
+        kappa = cohen_kappa_score(all_trgs, all_outs)
+        conf_matrix = confusion_matrix(all_trgs, all_outs)
+
+        print("Classification Report:\n", classification_report_result)
+        print("Accuracy:", accuracy)
+        print("Cohen's Kappa Score:", kappa)
+        print("Confusion Matrix:\n", conf_matrix)
+        # ----------------------------------------------------------------------
+
+        # Simpan hasilnya ke dalam file seperti yang sudah ada
         r = classification_report(all_trgs, all_outs, digits=6, output_dict=True)
         cm = confusion_matrix(all_trgs, all_outs)
         df = pd.DataFrame(r)
@@ -225,6 +238,7 @@ class BaseTrainer:
         cm_file_name = self.config["name"] + "_confusion_matrix.torch"
         cm_Save_path = os.path.join(save_dir, cm_file_name)
         torch.save(cm, cm_Save_path)
+
 
 
         # Uncomment if you want to copy some of the important files into the experiement folder
